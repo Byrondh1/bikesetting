bc2cfec4c984d642ac9fb8fc0221d51ae6cf340c
Byrondh1 <Byrondh1@users.noreply.github.com>
2026-05-28 01:32:31 +0000

    deploy: 50913a2a6d459ef0fd0d7aec6241d2a6ac0c2129

---
+++ _next/static/qP76vEF80aZ495bZAF6Tq/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/bikesetting//_next/:path+",
+        "destination": "/bikesetting/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
